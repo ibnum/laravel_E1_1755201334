28d4b0d2540a640c038f2b9379fa182fc155a687
ibnum <muhammadibnu1623@gmail.com>
2020-08-08 12:22:13 +0700

    Part 11 : Membuat Halaman Template

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -14,7 +14,7 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        return "Ini Halaman Mahasiswa (Index)";
+        return view('mahasiswa.index');
     }
 
     /**
